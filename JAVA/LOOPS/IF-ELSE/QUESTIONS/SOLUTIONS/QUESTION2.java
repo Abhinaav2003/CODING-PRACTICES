a37e1c16ee3def8895695e67579c57ee0cae7a89
Abhinaav2003 <144618349+Abhinaav2003@users.noreply.github.com>
2026-02-01 21:18:29 +0530

    QUESTION2

---
+++ JAVA/LOOPS/IF-ELSE/QUESTIONS/SOLUTIONS/QUESTION2.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class QUESTION2 
+{
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+
+        int num;
+
+        System.out.println("Enter an Number :");
+        num = sc.nextInt();
+
+        if(num%2==0)
+        {
+            System.out.println("Number is Even");
+        }
+        else
+        {
+            System.out.println("Number is Odd");
+        }
+        sc.close();
+    }
+}
