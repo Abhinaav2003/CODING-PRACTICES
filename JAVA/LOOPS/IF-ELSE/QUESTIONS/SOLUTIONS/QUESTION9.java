d5b254a993b532e909ba8f2fdfda34efc90bbb63
Abhinaav2003 <144618349+Abhinaav2003@users.noreply.github.com>
2026-02-01 23:00:09 +0530

    9

---
+++ JAVA/LOOPS/IF-ELSE/QUESTIONS/SOLUTIONS/QUESTION9.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class QUESTION9 
+{
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter an Number :");
+        int num = sc.nextInt();
+
+        if(num>0)
+        {
+            System.out.println("Number is Non-Zero");
+        }
+        else if(num<=0)
+        {
+            System.out.println("Number is Zero");
+        }
+        sc.close();
+    }
+}
