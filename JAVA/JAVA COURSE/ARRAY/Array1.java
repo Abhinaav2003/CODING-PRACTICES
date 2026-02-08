982514958e1d3572c9e63b4eec264bf20f297a29
Abhinaav2003 <144618349+Abhinaav2003@users.noreply.github.com>
2026-02-08 19:01:29 +0530

    a

---
+++ JAVA/JAVA COURSE/ARRAY/Array1.java
@@ -0,0 +1,18 @@
+public class Array1 
+{
+    public static void main(String[] args) 
+    {
+        int Arr[]  = new int[5];
+
+        Arr[0] =  1;
+        Arr[1] =  3;
+        Arr[2] =  5;
+        Arr[3] =  7;
+        Arr[4] =  18;
+
+        for(int i=0;i<Arr.length;i++)
+        {
+            System.out.println(Arr[i]);
+        }
+    }
+}
