14e67b0629633d7df146174c5a6d602fc52b2590
Abhinaav2003 <144618349+Abhinaav2003@users.noreply.github.com>
2026-02-10 20:28:01 +0530

    a

--- JAVA/JAVA COURSE/ARRAY/Array1.java
+++ JAVA/JAVA COURSE/ARRAY/Array1.java
@@ -1,15 +1,23 @@
 public class Array1 
 {
-
-    static int add(int a, int b) 
-    {
-        return a + b;
-    }
-
     public static void main(String[] args) 
     {
-        int result = add(5, 10);
-        
-        System.out.println("Sum = " + result);
+        int Arr[] = new int[10];
+
+        Arr[0]  = 1;
+        Arr[1]  = 1;
+        Arr[2]  = 1;
+        Arr[3]  = 1;
+        Arr[4]  = 1;
+        Arr[5]  = 1;
+        Arr[6]  = 1;
+        Arr[7]  = 10;
+        Arr[8]  = 1;
+        Arr[9]  = 1;
+
+        for(int i=0;i<Arr.length;i++)
+        {
+            System.out.println(Arr[i]);
+        }
     }
 }
