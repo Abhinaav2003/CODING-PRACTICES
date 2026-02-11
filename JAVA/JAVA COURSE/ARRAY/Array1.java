e38cf67fb9fae48c2893043570244a92cad0926b
Abhinaav2003 <144618349+Abhinaav2003@users.noreply.github.com>
2026-02-11 21:56:18 +0530

    a

--- JAVA/JAVA COURSE/ARRAY/Array1.java
+++ JAVA/JAVA COURSE/ARRAY/Array1.java
@@ -1,25 +1,16 @@
-import  java.util.Scanner;
+import java.util.LinkedList;
 
 public class Array1 
 {
-    public static void main(String[] args) 
-    {
-        Scanner sc = new Scanner(System.in);
-        int Arr[] = new int[10];
-        int i;
+  public static void main(String[] args) 
+  {
+    LinkedList<String> cars = new LinkedList<String>();
+    
+    cars.add("Volvo");
+    cars.add("BMW");
+    cars.add("Ford");
+    cars.add("Mazda");
 
-        System.out.println("Enter Array Elements :");
-        for(i=0;i<Arr.length;i++)
-        {
-            Arr[i] = sc.nextInt();
-        }
-
-        System.out.println("Entered Array Elements :");
-        for(i=0;i<Arr.length;i++)
-        {
-            System.out.println(Arr[i]);
-        }
-
-        sc.close();
-    }
+    System.out.println(cars);
+  }
 }
