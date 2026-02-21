77fbcea0d93308390f447d0e0785b7952582fbd0
Abhinaav2003 <144618349+Abhinaav2003@users.noreply.github.com>
2026-02-21 14:39:11 +0530

    a

---
+++ DSA/SORTING/Insertion_Sort.java
@@ -0,0 +1,28 @@
+package DSA.SORTING;
+
+
+public class Insertion_Sort 
+{
+    public static void main(String[] args) 
+    {
+        int arr[] = {5, 3, 8, 4, 2};
+        int n = arr.length;
+
+        for (int i = 1; i < n; i++) 
+        {
+            int key = arr[i];
+            int j = i - 1;
+
+            while (j >= 0 && arr[j] > key) 
+            {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+
+            arr[j + 1] = key;
+        }
+
+        for (int num : arr)
+            System.out.print(num + " ");
+    }
+}
