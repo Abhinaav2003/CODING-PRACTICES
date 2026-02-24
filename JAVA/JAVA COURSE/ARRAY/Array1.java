9b127652b3de7b68946b7f699d6333dc3100245f
Abhinaav2003 <144618349+Abhinaav2003@users.noreply.github.com>
2026-02-24 20:53:58 +0530

    a

--- JAVA/JAVA COURSE/ARRAY/Array1.java
+++ JAVA/JAVA COURSE/ARRAY/Array1.java
@@ -2,7 +2,7 @@ import java.util.LinkedList;
 
 public class Array1 
 {
-  public static void main(String[] args) 
+  public static void main(String[] args)
   {
     LinkedList<String> cars = new LinkedList<String>();
     
