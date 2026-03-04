d3d9cb8a03bc2a14cf5523548c049f4c0bbcea91
Abhinaav2003 <144618349+Abhinaav2003@users.noreply.github.com>
2026-03-04 16:14:37 +0530

    array

--- JAVA/JAVA COURSE/ARRAY/Array1.java
+++ JAVA/JAVA COURSE/ARRAY/Array1.java
@@ -1,16 +1,17 @@
-import java.util.LinkedList;
+import java.util.*;
 
 public class Array1 
 {
-  public static void main(String[] args)
-  {
-    LinkedList<String> cars = new LinkedList<String>();
-    
-    cars.add("Volvo");
-    cars.add("BMW");
-    cars.add("Ford");
-    cars.add("Mazda");
+    public static void main(String[] args) 
+    {
+        int[] intArray;
 
-    System.out.println(cars);
-  }
+        intArray = new int[3];
+
+        intArray[0] = 1;
+        intArray[1] = 2;
+        intArray[2] = 3;
+
+        System.out.println(Arrays.toString(intArray));
+    }
 }
